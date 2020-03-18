ce5a2946f7173faaf0a36fdfdadc8df709916ed5
Braden Mars <bradenmars@bradenmars.me>
2020-03-18 17:40:54 -0500

    feat(sentry): setup sentry integration

--- src/main.js
+++ src/main.js
@@ -30,6 +30,8 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import VueTagsInput from '@johmun/vue-tags-input';
+import * as Sentry from '@sentry/browser';
+import * as SentryIntegrations from '@sentry/integrations';
 import axios from 'axios';
 import Dropdown from 'bp-vuejs-dropdown';
 import detectBrowserLanguage from 'detect-browser-language';
@@ -111,6 +113,18 @@ Vue.filter('getColorForStatus', getColorForStatus);
 if (AuthService.getUser()) {
   axios.defaults.headers.common.Authorization = `Bearer ${AuthService.getToken()}`;
 }
+
+// Sentry Logging
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new SentryIntegrations.Vue({
+      Vue,
+      attachProps: true,
+      logErrors: process.env.NODE_ENV !== 'production',
+    }),
+  ],
+});
 
 axios.interceptors.response.use(
   function(response) {
